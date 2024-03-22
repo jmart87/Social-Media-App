84920fb0afd8fe00b2680dc3e4f3ee27bdb8fec9
Jessie Martinez <jmartinez8587@gmail.com>
2024-03-22 16:23:49 -0400

    Passed props to Post component

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ function App(){
 
         {/* Feed */}
 
-        <Post />
-        
+        <Post content="This is a test post!" />
+        <Post content="This is another test post!" />
+
     </div>
   )
 }
